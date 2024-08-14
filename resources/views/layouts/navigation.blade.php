5791f7db011106c34d0bae02757ca4aa905d13fe
OcidDev <ocid.asmanagement@gmail.com>
2024-08-14 08:54:53 +0700

    pembuatan routing pada project

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -16,6 +16,24 @@
                         {{ __('Dashboard') }}
                     </x-nav-link>
                 </div>
+
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link :href="route('admin.categories.index')" :active="request()->routeIs('admin.categories.index')">
+                        {{ __('Categories') }}
+                    </x-nav-link>
+                </div>
+
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link :href="route('admin.package_banks.index')" :active="request()->routeIs('admin.banks.index')">
+                        {{ __('Banks') }}
+                    </x-nav-link>
+                </div>
+
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link :href="route('admin.package_tours.index')" :active="request()->routeIs('admin.package_tours.index')">
+                        {{ __('Tours') }}
+                    </x-nav-link>
+                </div>
             </div>
 
             <!-- Settings Dropdown -->
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\PackageBankController;
+use App\Http\Controllers\PackageTourController;
+use App\Http\Controllers\PackageBookingController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -15,6 +19,36 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::middleware('can: checkout package')->group(function(){
+        Route::get('book/{packageTour:slug}', [FrontController::class, 'book'])->name('book');
+        Route::post('book/{packageTour:slug}', [FrontController::class, 'book_store'])->name('book.store');
+
+        Route::get('book/choose-bank/{packageBooking}/', [FrontController::class, 'choose_bank'])->name('choose.bank');
+        Route::patch('book/choose-bank/{packageBooking}/save', [FrontController::class, 'choose_bank_store'])->name('choose.bank.store');
+
+        Route::get('book/payment/{packageBooking}/save', [FrontController::class, 'book_payment'])->name('book.payment');
+        Route::patch('book/payment/{packageBooking}/save', [FrontController::class, 'book_payment_store'])->name('book.payment.store');
+
+        Route::patch('book-finish', [FrontController::class, 'book_finish'])->name('book.finish');
+    });
+    Route::prefix('admin')->name('admin.')->group(function(){
+        Route::middleware('can: manage categories')->group(function(){
+            Route::resource('categories', CategoryController::class);
+        });
+
+        Route::middleware('can: manage packages')->group(function(){
+            Route::resource('package_tours', PackageTourController::class);
+        });
+
+        Route::middleware('can: manage package banks')->group(function(){
+            Route::resource('package_banks', PackageBankController::class);
+        });
+
+        Route::middleware('can: manage transactions')->group(function(){
+            Route::resource('package_bookings', PackageBookingController::class);
+        });
+    });
 });
 
 require __DIR__.'/auth.php';
